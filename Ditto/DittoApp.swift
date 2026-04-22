e87e7d7e55a2cd6c6428bffc2462d15d427f8149
kkt6394 <kkt6394@gmail.com>
2026-04-22 21:09:01 +0900

    Initial Commit

---
+++ Ditto/DittoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DittoApp.swift
+//  Ditto
+//
+//  Created by 김기태 on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct DittoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DittoTests/DittoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DittoTests.swift
+//  DittoTests
+//
+//  Created by 김기태 on 4/22/26.
+//
+
+import Testing
+@testable import Ditto
+
+struct DittoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
